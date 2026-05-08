67ef70098c5237a1f87dca8c4873a571fad02f71
Yvan Emiliau <y.emiliau@gmail.com>
2026-05-08 10:16:14 +0000

    02: Add tab navigation with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
-import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return (
-    <>
-        <StatusBar style="auto" />
-        <Stack>
-            <Stack.Screen name="index" options={{ title: 'SkipLine' }} />
-            <Stack.Screen name="details" options={{ title: 'Details' }} />
-        </Stack>
-    </>
-  );
+    return (
+        <>
+            <StatusBar style="auto" />
+            <Tabs>
+                <Tabs.Screen name="index" options={{
+                    title: 'SkipLine', tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" size={size} color={color} />
+                    ),
+                }} />
+                <Tabs.Screen name="details" options={{
+                    title: 'Details', tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="information-circle-outline" size={size} color={color} />
+                    ),
+                }} />
+            </Tabs>
+        </>
+    );
 }
